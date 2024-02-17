940f017dd228b848a3c54b14b71e3394cd13212a
xihale <xihale.top@qq.com>
2024-02-17 21:39:38 +0800

    reflactor: directly use css modifly the display of Contributors list

--- src/components/Contributors/Contributors.tsx
+++ src/components/Contributors/Contributors.tsx
@@ -51,31 +51,29 @@ function Content() {
   });
 }
 
-const Contributors = () => {
-  const [modalVisible, setModalVisible] = useState(false);
 
-  const hide = () => {
-    setModalVisible(false);
-    document.body.style.overflow = "auto"; // scrollbar
-  };
-  const show = () => {
-    setModalVisible(true);
-    document.body.style.overflow = "hidden"; // scrollbar
-  };
+const hide = () => {
+  // setModalVisible(false);
+  const overlay = document.querySelector(".overlay") as HTMLElement;
+  overlay.style.transform = "translateX(-100%)";
+  document.body.style.overflow = "auto"; // scrollbar
+};
+export const show = () => {
+  // setModalVisible(true);
+  const overlay = document.querySelector(".overlay") as HTMLElement;
+  overlay.style.transform = "translateX(0)";
+  document.body.style.overflow = "hidden"; // scrollbar
+};
+
+const Contributors = ({children}:React.PropsWithChildren) => {
+  // const [modalVisible, setModalVisible] = useState(false);
 
   return (
     <>
-      {/* <div className="fooBtn" type="primary" onClick={_=>GoPage('/contributors/')}>
-        鸣谢名单
-      </div> */}
-      <div className="fooBtn" type="primary" onClick={show}>
-        鸣谢名单
-      </div>
-
       <div
-        style={{
-          transform: modalVisible ? "translateX(0)" : "translateX(-100%)",
-        }}
+        // style={{
+        //   transform: modalVisible ? "translateX(0)" : "translateX(-100%)",
+        // }}
         className="overlay"
       >
         <Return onClick={hide} />
