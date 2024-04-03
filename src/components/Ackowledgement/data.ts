bc68ef42b11346e1dac37e584827a52a6b3ccb54
CimiMoly <48214360+EnderMo@users.noreply.github.com>
2024-04-04 01:25:53 +0800

    Update data.ts

--- src/components/Ackowledgement/data.ts
+++ src/components/Ackowledgement/data.ts
@@ -1,5 +1,5 @@
 export const title = "3月赞助者(10r+)";
 
 export const names = [
-  "Jostein","Skuld_Cloud217","Li_wasmcwasmc","Lucas2333","梦竹","以珀瑞纳","A-bigqian","在在在","MEMUZE","悟小影"
+  "Jostein","Skuld_Cloud217","梦竹","Li_wasmcwasmc","Lucas2333","以珀瑞纳","A-bigqian","在在在","MEMUZE","悟小影"
 ];
