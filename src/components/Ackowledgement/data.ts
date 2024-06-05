ae083c51c2401da3785ae2bab209881de3ff3f2d
CimiMoly <48214360+EnderMo@users.noreply.github.com>
2024-06-05 23:56:43 +0800

    Update data.ts

--- src/components/Ackowledgement/data.ts
+++ src/components/Ackowledgement/data.ts
@@ -1,5 +1,5 @@
 export const title = "4月赞助者(10r+)";
 
 export const names = [
-  "sty_shrimp"
+  "sty_shrimp","空逝scs","Ultra_Greedier","nz","柠柚","👀","爱发电用户_nSmU","Do_Better","TCliquor","爱发电用户_yqBb"
 ];
